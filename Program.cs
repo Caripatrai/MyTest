cb69f0ef9d0b006b3c482f2b3123177a41b702a2
Caripatrai <lobogastonm@gmail.com>
2018-01-06 11:25:59 -0300

    My first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace Programa03_01
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//TODO: pedir numero
+			//variables
+			string dato;
+			int valor;
+			//pedir numero
+			Console.WriteLine("pasame un numero");
+			dato = Console.ReadLine ();
+			valor = Convert.ToInt32 (dato);
+
+			//mostrar	
+			Console.WriteLine (valor * 2);
+
+		}
+	}
+}
